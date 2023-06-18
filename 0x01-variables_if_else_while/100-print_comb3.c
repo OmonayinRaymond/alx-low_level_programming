5f91b05caedc85c07aec148cc02216e8e45f3461
OmonayinRaymond <omo9ray@gmail.com>
2023-06-18 00:31:50 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: A program to print all possible different
+ * combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = i + 1; j <= 9; j++)
+	{
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
