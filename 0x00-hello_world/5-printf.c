207521440609ccd5b8e29a6c073da6e34aed5821
OmonayinRaymond <omo9ray@gmail.com>
2023-06-18 06:00:49 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Staring point of program
+ *
+ * Description: A program to use printf
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
