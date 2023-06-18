fee63c431ff4c444963913f0eb884c1d548f03a8
OmonayinRaymond <omo9ray@gmail.com>
2023-06-18 01:08:02 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,22 @@ int main(void)
 {
 	int i, j;
 
+	int count = 0;
+
 	for (i = 0; i <= 9; i++)
 	{
 	for (j = i + 1; j <= 9; j++)
 	{
 	putchar(i + '0');
 	putchar(j + '0');
+
+	count = count + 1;
+
+	if (count < 44)
+	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	putchar('\n');
