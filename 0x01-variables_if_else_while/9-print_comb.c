b85a320f6e37ea9c70bb744a833040812c0b9bdc
OmonayinRaymond <omo9ray@gmail.com>
2023-06-17 23:22:34 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,19 +13,15 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
-	for (j = 0; j <= 9; j++)
-	{
 	putchar(i + '0');
-	putchar(j + '0');
+	putchar(',');
 	putchar(' ');
 	}
 
 	putchar('\n');
-	}
 
 	return (0);
 
