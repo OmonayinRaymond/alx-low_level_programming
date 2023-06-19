a4ecc06f208a8da73d7297767ddb08e3672a4196
OmonayinRaymond <omo9ray@gmail.com>
2023-06-19 02:04:04 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,9 +20,9 @@ int main(void)
 	{
 	for (j = 0; j <= 9; j++)
 	{
-	for (k = 0; k <= 9; k++)
+	for (k = j + 1; k <= 9; k++)
 	{
-	for (l = 0; l <= 9; l++)
+	for (l = k + 1; l <= 9; l++)
 	{
 	putchar(i + '0');
 	putchar(j + '0');
