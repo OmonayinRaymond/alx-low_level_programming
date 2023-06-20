9b6d2be26b0007f84e4a3b78941009afc786487d
OmonayinRaymond <omo9ray@gmail.com>
2023-06-19 18:33:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@
 
 int main(void)
 {
-	const char A[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19";
+	const char A[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(2, A, sizeof(A) - 1);
 
