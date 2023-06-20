37ba951f3dc7bd9868a6dba62c50a31a0286925e
OmonayinRaymond <omo9ray@gmail.com>
2023-06-19 17:34:44 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Desciption: Aprogram that prints exactly and that piece of art is useful"
+ *  - Dora Korpar, 2015-10-19
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	const char Arr[] = "and that piece of art is useful\""
+	"- Dora Korpar, 2015-10-19\n";
+
+	write(2, Arr, sizeof(Arr) - 1);
+
+	return (1);
+}
